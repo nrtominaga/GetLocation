8925193c2d3a69814d15f1211fa8e1cb5bf7d1ee
Naoki Tominaga <u0876779@utah.edu>
2019-07-15 17:16:43 -0600

    forgot to add root view controller

---
+++ Get Location/GetLocationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GetLocationViewController.swift
+//  Get Location
+//
+//  Created by tominaga on 7/15/19.
+//  Copyright © 2019 Naoki Tominaga. All rights reserved.
+//
+
+import UIKit
+
+class GetLocationViewController: UIViewController {
+    
+}
