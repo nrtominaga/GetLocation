a2bc57726a5f4b8788783aaacc1d0ac8cd7d97f8
Naoki Tominaga <u0876779@utah.edu>
2019-07-15 17:11:30 -0600

    first commit

---
+++ Get Location/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Get Location
+//
+//  Created by tominaga on 7/15/19.
+//  Copyright © 2019 Naoki Tominaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
