30345a179637de9531b10f26358e8c3a5969dca0
Naoki Tominaga <u0876779@utah.edu>
2019-07-19 16:19:11 -0600

    screen actually set up?

--- Get Location/GetLocationViewController.swift
+++ Get Location/GetLocationViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class GetLocationViewController: UIViewController {
+    
+    var getLocationView: GetLocationView {
+        return view as! GetLocationView
+    }
+    
     override func loadView() {
         view = GetLocationView()
+    }
+    
+    override func viewDidLoad() {
+        getLocationView.getLocationButton.addTarget(self, action: #selector(getLocationButtonPressed), for: .touchUpInside)
+    }
+    
+    @objc func getLocationButtonPressed() {
+        print("hi")
     }
 }
